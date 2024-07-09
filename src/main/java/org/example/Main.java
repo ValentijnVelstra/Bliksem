0106a713e0a27a5a89c30c74c3ec4f37ad1ebbd1
Valentijn <velstravalentijn@gmail.com>
2024-07-09 10:30:46 +0200

    berekening voor km/h en ft/s toegevoegd. bereking voor tijd tot storm bij je is. printf gebruikt ipv print ln.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,16 +10,27 @@ public class Main {
         Xm = 343; // variabele voor berekening
         Xft = 1125; // variabele voor berekening
         int x = 0; // voor een loop
+        double distance2 = 0;
         while (x == 0) {
             Scanner sec = new Scanner(System.in);  // Create a Scanner object
             System.out.println("Enter seconds between lighting and thunder:");
             Double seconds = sec.nextDouble();  // Read user input
             if (FTorKM == 1){  // if statement om te kijken of het in km of ft moet.
-                seconds = (seconds * Xm)/1000;
-                System.out.println(seconds + "KM");
+                double distance = (seconds * Xm)/1000;
+                double kmh = (((distance2 - distance)/seconds)* 3.6);
+                double speed = (distance2/kmh);
+                distance2 = distance;
+                System.out.println(distance + "km");
+                System.out.printf("%.2f km/h%n", kmh);
+                System.out.printf("%.2f seconds%n", speed);
             }else {
-                seconds = (seconds * Xft)/1000;
-                System.out.println(seconds + "ft");
+                double distance = (seconds * Xft);
+                double fts = (((distance2 - distance)/seconds)* 3.6);
+                double speed = (distance2/fts);
+                distance2 = distance;
+                System.out.println(distance + "ft");
+                System.out.printf("%.2f ft/h%n", fts);
+                System.out.printf("%.2f seconds%n", speed);
             }
             x++;
         x--;
